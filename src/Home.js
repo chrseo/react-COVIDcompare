3e3cb1b76d2f3c889457c97425a88f3d0e1b1824
Christopher Seo <christopherseo@berkeley.edu>
2020-06-02 15:31:39 -0700

    added gradient and footer section

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import HomeBody from "./HomeBody";
+import "./Home.css";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <HomeBody />
+        <div className="notFooter">
+          <Header />
+          <HomeBody />
+        </div>
+        <div className="gradient"></div>
+        <div className="footerBar">
+          <p>Created by Chris Seo in quarantine</p>
+        </div>
       </div>
     );
   }
